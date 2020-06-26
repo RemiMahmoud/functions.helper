27c48af1dbc1dc68b8dd6b260ea68c0037988e62
RemiMahmoud <mahmoudremi@gmail.com>
2020-06-26 16:33:20 +0200

    Update with dplyr 1.0

--- R/sample_n_of.R
+++ R/sample_n_of.R
@@ -4,7 +4,7 @@
 #' @param size the number  subgroups you want  sample
 #' @param ... the column names  the subgroup you want  sample
 
-#' @importFrom dplyr group_by quos group_indices filter
+#' @importFrom dplyr group_by quos cur_group_id filter mutate pull
 #' @importFrom magrittr %>%
 #' @description Taken and adapted from https://www.tjmahr.com/sample-n-groups/
 #' @return tibble with elements belonging  the sampled subgroups
@@ -17,12 +17,14 @@
 #' mutate(rowid = 1:n()) %>%
 #' group_by(cyl) %>%
 #' do(sample_n_of(., 2,rowid))
+
 sample_n_of <- function(data, size,  ...) {
 dots <- quos(...)
 
 group_ids <- data %>%
     group_by(!!! dots, .drop = FALSE) %>%
-    group_indices
+    mutate(grp_id = cur_group_id()) %>%
+    pull(grp_id)
 
 if(length(unique(group_ids))< size){
   warning(paste0("Argument size large than the population ", dots, ". Setting size to the size of the population"))
